66bca9626d0e94c7f15957c177e7e6e0c9b7afc0
prggd2 <prg.gd2@hotmail.com>
2015-10-23 16:44:07 -0500

    Listo 2

--- solucion_vs/solucion_VS/app/Form1.cs
+++ solucion_vs/solucion_VS/app/Form1.cs
@@ -15,5 +15,10 @@ namespace app
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Holas");
+        }
     }
 }
